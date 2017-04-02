8fef12b154bd60f7b9f4ab9028cdde97edc2db5e
Rishi Karri <rishikarri1@gmail.com>
2017-04-02 10:38:09 -0400

    created first component

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,17 @@
-//start this from scratch
+// index.ios.js - place code in here for ios	
+
+// import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+// create a component -
+const App = () => {
+	return (
+		<Text>It is lit</Text>
+	)
+}
+
+
+// Render it to the device
+
