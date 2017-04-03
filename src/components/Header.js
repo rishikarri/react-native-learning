54d208257751b2fca98db172c87e3c08b6de4c24
Rishi Karri <rishikarri1@gmail.com>
2017-04-02 22:55:39 -0400

    added styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
 // Import libraries for making a component - 
 
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 // Make a component - 
 
 const Header = () => {
-	return <Text>Albums!</Text>;
+	const { textStyle, viewStyle } = styles; 
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
+	);
+};
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2px },
+		shadowOpacity: 0.2
+	},
+	textStyle: {
+		fontSize: 20,		
+	}
+
 };
 
 // Make the component available to other parts of the app
