a256e8af014611fcf6578dbba4c7ecf7a13acf54
Rishi Karri <rishikarri1@gmail.com>
2017-04-04 07:06:46 -0400

    albumlist updated

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,23 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+	// we have an empty list of albums when we first start off
+	//declare state in a new way so that we have an empty array to render at first (this will be replaced by a populated array)
+	state = { 
+		albums: [] 
+	}; 
 
 	componentWillMount() {
 		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-		.then(response => console.log(response));
+		.then(response => 
+			this.setState({
+				albums: response.data
+			})
+		);
 	}
 	render(){
+
+		console.log(this.state);
 		return (
 			<View>
 				<Text> AlbumList!!!</Text>
