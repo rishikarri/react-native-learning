ebb7d0d34b5b80d2e89f812690b169e0d3d01c36
Rishi Karri <rishikarri1@gmail.com>
2017-04-05 14:44:47 -0500

    added images to the page

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -9,22 +9,34 @@ import CardSection from './CardSection.js';
 const AlbumDetail = ({ album }) => {
 
 	// i want my title artist and thumbnail image from the album object
-	const { title, artist, thumbnail_image } = album;
+	const { title, artist, thumbnail_image, image } = album;
+
+	const {
+		thumbnailStyle,
+		headerContentStyle,
+		thumbnailContainerStyle,
+		headerTextStyle, 
+		imageStyle
+	} = styles;
 
 	return (
 		<Card>
 			<CardSection>
-				<View>
+				<View style={thumbnailContainerStyle}>
 					<Image
-						style = {styles.thumbnailStyle} 
+						style = {thumbnailStyle} 
 						source={{ uri: thumbnail_image }} 
 					/>
 				</View>
 
-				<View style = {styles.headerContentStyle}>
-					<Text>{title}</Text>
+				<View style = {headerContentStyle}>
+					<Text style={headerTextStyle}>{title}</Text>
 					<Text>{artist}</Text>
 				</View>
+			</CardSection>
+
+			<CardSection>
+				<Image style={imageStyle} source = {{ uri: image }} />
 			</CardSection>
 		</Card>
 	);
@@ -39,6 +51,20 @@ const styles = {
 	thumbnailStyle: {
 		height: 50, 
 		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	}, 
+	headerTextStyle: {
+		fontSize: 18
+	}, 
+	imageStyle: {
+		height: 300, 
+		flex: 1, 
+		width: null
 	}
 };
 
