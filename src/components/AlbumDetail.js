6af441da9a457e621f599bb04b173e7f3ea55c1b
Rishi Karri <rishikarri1@gmail.com>
2017-04-05 12:39:28 -0500

    added cardsection component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card.js';
+import CardSection from './CardSection.js';
 
 // functional component
 
 // need to receive props
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+	// i want my title artist and thumbnail image from the album object
+	const { title, artist, thumbnail_image } = album;
+
 	return (
 		<Card>
-			<Text>{props.album.title}</Text>
+			<CardSection>
+				<View>
+					<Image source={{ uri: props.album.thumbnail_image }} />
+				</View>
+
+				<View style = {styles.headerContentStyle}>
+					<Text>{props.album.title}</Text>
+					<Text>{props.album.artist}</Text>
+				</View>
+			</CardSection>
 		</Card>
 	);
 
 };
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	}
+}
 
 export default AlbumDetail;
